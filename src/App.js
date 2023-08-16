7e1611fec180e0ad1f5adaaeeb25f2f8ca7b6760
DimitrusB <dimitrusboss@mail.ru>
2023-08-17 00:11:57 +0300

    createref

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { createRef, useEffect }  from "react";
 import "./App.css";
 import { NavMenu } from "./components/NavMenu";
 import { AudioPlayer } from "./components/Player";
@@ -5,15 +6,32 @@ import { Center } from "./components/center";
 import { Panelplaylist } from "./components/rightPanel";
 
 function App() {
+  const linkRef = createRef();
+
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    console.log('Сработало')
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+      console.log('Сработало')
+    };
+  }, []);
+
+  const handleClickOutside = (event) => {
+    if (linkRef.current && !linkRef.current.contains(event.target)) {
+      this.setOpen(false)
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="wrapper">
-          <div className="container">
+          <div className="container" ref={linkRef} >
             <main className="main">
               <NavMenu></NavMenu>
-              <Center></Center>
+              <Center ></Center>
               <Panelplaylist></Panelplaylist>
             </main>
             <AudioPlayer></AudioPlayer>
