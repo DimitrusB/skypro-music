c9e9081ff56d812c4837bfa2799dd236d46f7db9
DimitusB <dimitrusboss@mail.ru>
2023-08-24 14:14:51 +0300

    player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,358 @@
+import styled from 'styled-components';
 import iconSprite from '../img/icon/sprite.svg';
-import './player.css';
 import React, { useState, useEffect } from 'react';
 
+const StyledMainBar = styled.div`
+position: absolute;
+bottom: 0;
+left: 0;
+width: 100%;
+background: rgba(28, 28, 28, 0.5);
+`
+const StyledMainBarContent = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: vertical;
+-webkit-box-direction: normal;
+-ms-flex-direction: column;
+flex-direction: column;
+`
+const StyledBarPlayerProgress = styled.div`
+width: 100%;
+height: 5px;
+background: #2e2e2e;
+`
+const StyledBarPlayerBlock = styled.div`
+height: 73px;
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+-webkit-box-pack: justify;
+-ms-flex-pack: justify;
+justify-content: space-between;
+`
+const StyledBarPlayer = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+-webkit-box-pack: start;
+-ms-flex-pack: start;
+justify-content: flex-start;
+`
+
+const StyledBarPlayerControls = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+padding: 0 27px 0 31px;
+`
+const StyledPlayerBtnSvg = styled.svg`
+${({ butsvg }) =>
+butsvg === 'prev' &&
+`
+width: 15px;
+height: 14px;
+`};
+
+${({ butsvg }) =>
+butsvg === 'play' &&
+`
+width: 22px;
+height: 20px;
+fill: #d9d9d9;
+`};
+
+${({ butsvg }) =>
+butsvg === 'next' &&
+`
+width: 15px;
+height: 14px;
+fill: inherit;
+stroke: #d9d9d9;
+`};
+
+${({ butsvg }) =>
+butsvg === 'repeat' &&
+`
+width: 18px;
+height: 12px;
+fill: transparent;
+stroke: #696969;
+`};
+${({ butsvg }) =>
+butsvg === 'shuffle' &&
+`
+width: 19px;
+height: 12px;
+fill: transparent;
+stroke: #696969;
+`};
+`
+const StyledPlayerBtn = styled.div`
+padding: 5px;
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+${({ butt }) =>
+butt === 'prev' &&
+`
+margin-right: 23px;
+`};
+
+${({ butt }) =>
+butt === 'play' &&
+`
+margin-right: 23px;
+`};
+
+${({ butt }) =>
+butt === 'next' &&
+`
+margin-right: 28px;
+fill: #a53939;
+`};
+
+${({ butt }) =>
+butt === 'repeat' &&
+`
+margin-right: 24px;
+`};
+${({ butt }) =>
+butt === 'shuffle' &&
+`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+`};
+`
+
+const StyledPlayerTrackPlay = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+`
+
+const StyledPlayerTrackPlayContain = styled.div`
+width: auto;
+display: -ms-grid;
+display: grid;
+-ms-grid-columns: auto 1fr;
+grid-template-columns: auto 1fr;
+grid-template-areas: "image author" "image album";
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+`
+const StyledTrackPlayImage = styled.div`
+  width: 51px;
+  height: 51px;
+  background-color: #313131;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  justify-content: center;
+  margin-right: 12px;
+  -ms-grid-row: 1;
+  -ms-grid-row-span: 2;
+  -ms-grid-column: 1;
+  grid-area: image;
+
+  ${({ isLoading }) =>
+    isLoading &&
+    `
+    background: linear-gradient(90deg, #313131 25%, #272727, #201f1f 75%);
+    background-size: 200% 100%;
+    animation: loading 1.5s infinite;
+    width: 51px;
+    height: 51px;
+  `}
+
+  @keyframes loading {
+    0% {
+      background-position: 200% 0;
+    }
+    100% {
+      background-position: -200% 0;
+    }
+  }
+`
+
+const StyledTrackPlaySvg = styled.svg`
+width: 18px;
+height: 17px;
+fill: transparent;
+stroke: #4e4e4e;
+${({ isLoading }) => isLoading &&
+` 
+visibility: hidden;
+
+`
+}
+`
+const StyledTrackPlayAuthor = styled.div`
+-ms-grid-row: 1;
+-ms-grid-column: 2;
+grid-area: author;
+min-width: 49px;
+${({ isLoading }) => isLoading &&
+` 
+background: linear-gradient(90deg, #313131 25%, #272727, #201f1f 75%);
+background-size: 200% 100%;
+animation: loading 1.5s infinite;
+width: 65px;
+height: 19px;
+
+`
+}
+`
+const StyledTrackPlayAuthorLink = styled.a`
+font-style: normal;
+font-weight: 400;
+font-size: 16px;
+line-height: 24px;
+color: #ffffff;
+white-space: nowrap;
+${({ isLoading }) => isLoading &&
+` 
+visibility: hidden;
+
+`
+}
+`
+
+const StyledTrackPlayAlbum = styled.div`
+-ms-grid-row: 2;
+-ms-grid-column: 2;
+grid-area: album;
+min-width: 49px;
+${({ isLoading }) => isLoading &&
+` 
+background: linear-gradient(90deg, #313131 25%, #272727, #201f1f 75%);
+background-size: 200% 100%;
+animation: loading 1.5s infinite;
+width: 65px;
+height: 19px;
+
+`
+}
+`
+const StyledTrackPlayAlbumLink = styled.a`
+font-style: normal;
+font-weight: 400;
+font-size: 13px;
+line-height: 24px;
+color: #ffffff;
+${({ isLoading }) => isLoading &&
+` 
+visibility: hidden;
+
+`
+}
+`
+const StyledBarVolumeBlock = styled.div`
+width: auto;
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+padding: 0 92px 0 0;
+`
+const StyledVolumeContent = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+-webkit-box-pack: end;
+-ms-flex-pack: end;
+justify-content: end;
+`
+const StyledVolumeImage = styled.div`
+width: 13px;
+height: 18px;
+margin-right: 17px;
+`
+const StyledVolumeSvg = styled.svg`
+width: 13px;
+height: 18px;
+fill: transparent;
+`
+const StyledVolumeProgress = styled.div`
+width: 109px;
+`
+const StyledTrackDislike = styled.div`
+display: -webkit-box;
+display: -ms-flexbox;
+display: flex;
+-webkit-box-orient: horizontal;
+-webkit-box-direction: normal;
+-ms-flex-direction: row;
+flex-direction: row;
+-webkit-box-align: center;
+-ms-flex-align: center;
+align-items: center;
+margin-left: 26%;
+`
+const StyledVolumeProgressLine = styled.input`
+width: 109px;
+`
+const StyledTracPlayLike = styled.div`
+padding: 5px;
+`
+const StyledTracPlayDis = styled.div`
+padding: 5px;
+margin-left: 28.5px;
+`
+const StyledTracPlayLikeSvg = styled.svg`
+width: 14px;
+height: 12px;
+fill: transparent;
+stroke: #696969;
+`
+const StyledTracPlayDisSvg = styled.svg`
+width: 14.34px;
+height: 13px;
+fill: transparent;
+stroke: #696969;
+`
 export function AudioPlayer (){
 
   const [isLoading, setIsLoading] = useState(true);
@@ -15,94 +366,93 @@ export function AudioPlayer (){
   }, []);
 
     return (
-        <div className="bar">
-        <div className="bar__content">
-          <div className="bar__player-progress"></div>
-          <div className="bar__player-block">
-            <div className="bar__player player">
-              <div className="player__controls">
-                <div className="player__btn-prev">
-                  <svg className="player__btn-prev-svg" alt="prev">
+        <StyledMainBar>
+        <StyledMainBarContent>
+          <StyledBarPlayerProgress></StyledBarPlayerProgress>
+          <StyledBarPlayerBlock>
+            <StyledBarPlayer>
+              <StyledBarPlayerControls>
+              <StyledPlayerBtn butt = 'prev'>
+                  <StyledPlayerBtnSvg butsvg = 'prev' alt="prev">
                   <title>Предыдущий трек</title>
                     <use xlinkHref={`${iconSprite}#icon-prev`}></use>
-                  </svg>
-                </div>
-                <div className="player__btn-play _btn">
-                  <svg className="player__btn-play-svg" alt="play" >
+                  </StyledPlayerBtnSvg>
+                </StyledPlayerBtn>
+                <StyledPlayerBtn butt = 'play'>
+                  <StyledPlayerBtnSvg butsvg = 'play' alt="play" >
                   <title>Воспроизведение</title>
                     <use xlinkHref={`${iconSprite}#icon-play`} ></use>
-                  </svg>
-                </div>
-                <div className="player__btn-next">
-                  <svg className="player__btn-next-svg" alt="next">
+                  </StyledPlayerBtnSvg>
+                </StyledPlayerBtn>
+                <StyledPlayerBtn butt = 'next'>
+                  <StyledPlayerBtnSvg butsvg = 'next' alt="next">
                   <title>Следующий трек</title>
                     <use xlinkHref={`${iconSprite}#icon-next`}></use>
-                  </svg>
-                </div>
-                <div className="player__btn-repeat _btn-icon">
-                  <svg className="player__btn-repeat-svg" alt="repeat">
+                  </StyledPlayerBtnSvg>
+                </StyledPlayerBtn>
+                <StyledPlayerBtn butt = 'repeat'>
+                  <StyledPlayerBtnSvg butsvg = 'repeat' alt="repeat">
                   <title>Повтор</title>
                     <use xlinkHref={`${iconSprite}#icon-repeat`}></use>
-                  </svg>
-                </div>
-                <div className="player__btn-shuffle _btn-icon">
-                  <svg className="player__btn-shuffle-svg" alt="shuffle">
+                  </StyledPlayerBtnSvg>
+                </StyledPlayerBtn>
+                <StyledPlayerBtn butt = 'shuffle'>
+                  <StyledPlayerBtnSvg butsvg = 'shuffle' alt="shuffle">
                   <title>Случайный порядок</title>
                     <use xlinkHref={`${iconSprite}#icon-shuffle`}></use>
-                  </svg>
-                </div>
-              </div>
+                  </StyledPlayerBtnSvg>
+                </StyledPlayerBtn>
+              </StyledBarPlayerControls>
 
-              <div className="player__track-play track-play">
-                <div className="track-play__contain">
-                  <div className={`track-play__image ${isLoading ? "skeleton__image" : ""}`}>
-                    <svg className={`track-play__svg ${isLoading ? "hidden-text" : ""}`} alt="music">
+              <StyledPlayerTrackPlay>
+                <StyledPlayerTrackPlayContain>
+                <StyledTrackPlayImage isLoading={isLoading}>
+                    <StyledTrackPlaySvg isLoading={isLoading} alt="music">
                       <use xlinkHref={`${iconSprite}#icon-note`}></use>
-                    </svg>
-                  </div>
-                  <div className={`track-play__author ${isLoading ? "skeleton__authorpl" : ""}`}>
-                    <a className={`track-play__author-link ${isLoading ? "hidden-text" : ""}`} href="http://"
-                      >Баста</a>
-                  </div>
-                  <div className={`track-play__album ${isLoading ? "skeleton__albumpl" : ""}`}>
-                    <a className={`track-play__album-link ${isLoading ? "hidden-text" : ""}`} href="http://">Ты та...</a>
-                  </div>
-                </div>
+                    </StyledTrackPlaySvg>
+                  </StyledTrackPlayImage>
+                  <StyledTrackPlayAuthor isLoading={isLoading}>
+                    <StyledTrackPlayAuthorLink isLoading={isLoading}  href="http://"
+                      >Баста</StyledTrackPlayAuthorLink>
+                  </StyledTrackPlayAuthor>
+                  <StyledTrackPlayAlbum isLoading={isLoading}>
+                    <StyledTrackPlayAlbumLink isLoading={isLoading}href="http://">Ты та...</StyledTrackPlayAlbumLink>
+                  </StyledTrackPlayAlbum>
+                </StyledPlayerTrackPlayContain>
 
-                <div className="track-play__like-dis">
-                  <div className="track-play__like _btn-icon">
-                    <svg className="track-play__like-svg" alt="like">
+                <StyledTrackDislike>
+                  <StyledTracPlayLike>
+                    <StyledTracPlayLikeSvg alt="like">
                       <use xlinkHref={`${iconSprite}#icon-note`}></use>
-                    </svg>
-                  </div>
-                  <div className="track-play__dislike _btn-icon">
-                    <svg className="track-play__dislike-svg" alt="dislike">
+                    </StyledTracPlayLikeSvg>
+                  </StyledTracPlayLike>
+                  <StyledTracPlayDis>
+                    <StyledTracPlayDisSvg alt="dislike">
                       <use
                         xlinkHref={`${iconSprite}#icon-dislike`}
                       ></use>
-                    </svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="bar__volume-block volume">
-              <div className="volume__content">
-                <div className="volume__image">
-                  <svg className="volume__svg" alt="volume">
+                    </StyledTracPlayDisSvg>
+                  </StyledTracPlayDis>
+                </StyledTrackDislike>
+              </StyledPlayerTrackPlay>
+            </StyledBarPlayer>
+            <StyledBarVolumeBlock>
+              <StyledVolumeContent>
+                <StyledVolumeImage>
+                  <StyledVolumeSvg alt="volume">
                     <use xlinkHref={`${iconSprite}#icon-volume`}></use>
-                  </svg>
-                </div>
-                <div className="volume__progress _btn">
-                  <input
-                    className="volume__progress-line _btn"
+                  </StyledVolumeSvg>
+                </StyledVolumeImage>
+                <StyledVolumeProgress>
+                  <StyledVolumeProgressLine
                     type="range"
                     name="range"
                   />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                </StyledVolumeProgress>
+              </StyledVolumeContent>
+            </StyledBarVolumeBlock>
+          </StyledBarPlayerBlock>
+        </StyledMainBarContent>
+      </StyledMainBar>
     )
 }
