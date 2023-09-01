68330ed75672ec595d94f4057ad167792b522223
DimitusB <dimitrusboss@mail.ru>
2023-09-01 23:38:19 +0300

    404

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { SignUp } from "./pages/signup"
 import { MainPage } from "./pages/main";
 import { SignIn } from "./pages/signin";
+import { NotFoundPage } from "./pages/not_found";
 
 export const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
       <Route path="/" element={<MainPage />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
