3f70684872457eb086301695f0dc9053b49fa3b7
DimitusB <dimitrusboss@mail.ru>
2023-09-06 17:05:05 +0300

    api

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import { AppRoutes } from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import UserContext from '../src/components/UserContext';
 
+
 function App() {
   const [email, setEmail] = useState('');
   const resetEmail = () => {
     setEmail('');
   };
+
+
 
   return (
     <UserContext.Provider value={{ email, setEmail,resetEmail }}>
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,11 +1,20 @@
 
 import { NavMenu } from "../../components/NavMenu";
 import { AudioPlayer } from "../../components/Player";
+import { getAllTracks } from "../../components/api";
 import { Center } from "../../components/center";
 import { Panelplaylist } from "../../components/rightPanel";
 import * as S from "./Main.style" 
 
 export function MainPage (){
+  getAllTracks()
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(`Error fetching data: ${error}`);
+  });
+
     return(
 <header className="App-header">
           <S.Wrapper>
