71111d901f9fc1928d007fb301b1633a86788298
DimitrusB <dimitrusboss@mail.ru>
2023-09-18 22:48:42 +0300

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import trackReducer from './reducers/trackReducer';
 
-const rootReducer = combineReducers({
+const rootReducer = {
   track: trackReducer,
-});
+};
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+});
 
 export default store;
