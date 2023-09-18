e852ac3f3176f8c26c3b7c7da383e13019fd21c0
dmitriy <admin@silverboats.ru>
2023-09-18 16:21:34 +0300

    genre

--- src/components/genreFilter.js
+++ src/components/genreFilter.js
@@ -1,39 +1,49 @@
-import React, { useState} from "react";
-import * as S from "./styled/Filters.style"
-export function GenreFilter(props) {
+import React, { useEffect, useState } from "react";
+import { getAllTracks } from "../api";
+import * as S from "./styled/Filters.style";
 
+export function GenreFilter(props) {
   const { id, name, onClick, isOpen } = props;
   const [to, setTo] = useState("");
+  const [genres, setGenres] = useState([]);
 
   const toggleDropdown = () => {
     onClick(id);
   };
 
-  const genres = [
-    { value: "all", label: "Все" },
-    { value: "Shanson", label: "Шансон" },
-    { value: "HH", label: "Хип-Хоп" },
-    { value: "Jazz", label: "Джаз" },
-    { value: "Reggy", label: "Регги" },
-  ];
+  useEffect(() => {
+    // Получение данных из API.
+    getAllTracks()
+      .then((data) => {
+        const uniqueGenres = [...new Set(data.map((genre) => genre.genre))];
+        setGenres(uniqueGenres);
+      })
+      .catch((error) => {
+        alert(`Ошибка получения данных с сервера: ${error}`);
+      });
+  }, []);
 
   return (
-    <S.Button
-    type="button"
-    onClick={toggleDropdown}>
-      <S.Choose isOpen={isOpen}>
-        {to || name}
-      </S.Choose>
+    <S.Button type="button" onClick={toggleDropdown}>
+      <S.Choose isOpen={isOpen}>{to || name}</S.Choose>
       {isOpen && (
         <S.Options>
+          <S.Option
+            key="all"
+            onClick={() => {
+              setTo("all");
+            }}
+          >
+            Все
+          </S.Option>
           {genres.map((genre) => (
             <S.Option
-              key={genre.value}
+              key={genre}
               onClick={() => {
-                setTo(genre.label);
+                setTo(genre);
               }}
             >
-              {genre.label}
+              {genre}
             </S.Option>
           ))}
         </S.Options>
