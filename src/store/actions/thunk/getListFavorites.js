1f02f339da21388d5f67748cacdafd7c0e426e3f
DimitrusB <dimitrusboss@mail.ru>
2023-10-04 23:40:37 +0300

    fix

--- src/store/actions/thunk/getListFavorites.js
+++ src/store/actions/thunk/getListFavorites.js
@@ -1,25 +1,33 @@
 import { refreshToken } from "../../../api";
 
 export const getAllFavoriteTracks = (token, tokenRefresh) => {
-    return function(dispatch) {
-        return fetch("https://skypro-music-api.skyeng.tech/catalog/track/favorite/all/", {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        })
-        .then(response => {
-          if (response.status === 401) {
-              return refreshToken(tokenRefresh)
-              .then(newToken => getAllFavoriteTracks(newToken.access, tokenRefresh))
-          }
-
-          if (!response.ok) throw new Error(response.statusText);
-          return response.json();
-        })
-        .then(
-            json => dispatch({ type: "FETCH_FAVORITES_SUCCESS", payload: json }),
-            error => dispatch({ type: "FETCH_FAVORITES_ERROR", error: error.toString() })
-        );
+    return (dispatch) => {
+      return makeRequestWithTokenRefresh('https://skypro-music-api.skyeng.tech/catalog/track/favorite/all/', token, tokenRefresh, dispatch);
     };
-}
+  }
+  
+  const makeRequestWithTokenRefresh = (url, token, tokenRefresh, dispatch, retryCount = 0) => {
+    return fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then(response => {
+        if(response.status === 401 && retryCount === 0) {
+          // Trying to refresh the token
+          return refreshToken(tokenRefresh)
+              .then((newToken) => makeRequestWithTokenRefresh(url, newToken.access, tokenRefresh, dispatch, retryCount + 1));
+        }
+        if (!response.ok) throw new Error(response.statusText);
+        return response.json();
+      })
+      .then(
+          json => dispatch({ type: "FETCH_FAVORITES_SUCCESS", payload: json }),
+          error => console.error('Error in fetching', error) // logging the error
+      )
+      .catch(error => {
+          console.error('Unexpected error in makeRequestWithTokenRefresh', error); 
+          dispatch({ type: "FETCH_FAVORITES_ERROR", error: error.toString() });
+      });
+  }
